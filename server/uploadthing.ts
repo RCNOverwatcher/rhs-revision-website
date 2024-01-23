7c779ad339c0881f7dceddad941c7d7fb2e0a51b
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-01-23 12:09:05 +0000

    suppress await eslint

--- server/uploadthing.ts
+++ server/uploadthing.ts
@@ -13,6 +13,7 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ blob: { maxFileSize: "128MB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
+      // eslint-disable-next-line @typescript-eslint/require-await
     .middleware(async ({ req, res }) => {
       // This code runs on your server before upload
       const user = auth(req, res);
@@ -23,6 +24,7 @@ export const ourFileRouter = {
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
+      // eslint-disable-next-line @typescript-eslint/require-await
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);

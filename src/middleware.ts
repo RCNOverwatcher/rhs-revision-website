3fdd053d60cb4cd829fdfa7c82bf24244d922320
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-01-23 16:00:05 +0000

    add back middleware

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/api/uploadthing",
+    "/",
+    "/api/fetchMaterials",
+    "/api/fetchMaterialsBySubject",
+    "/api/fetchMaterialsBySubject_alevel",
+    "/api/fetchMaterialsBySubject_gcse",
+  ],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
